3d732cb35451679f187d28941a908cf60b8aafc7
davidhe-usc <davidhe@usc.edu>
2025-01-26 12:06:48 -0800

    Attached CanCapture boolean

--- Assets/Scripts/Managers/CaptureManager.cs
+++ Assets/Scripts/Managers/CaptureManager.cs
@@ -9,37 +9,41 @@ public class CaptureManager : MonoBehaviour
     [SerializeField] private GameObject captureHead;
     [SerializeField] private CaptureLine captureLine;
     private CaptureLine currentLine;
-    
+    public bool canCapture;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canCapture = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        captureHead.transform.position = mousePosition;
-
-        if (Input.GetMouseButtonDown(0))
+        if (canCapture)
         {
-            currentLine = Instantiate(captureLine, mousePosition, Quaternion.identity);
-        }
+            Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            captureHead.transform.position = mousePosition;
 
-        else if (Input.GetMouseButton(0) && currentLine != null)
-        {
-            currentLine.SetPosition(mousePosition);
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentLine = Instantiate(captureLine, mousePosition, Quaternion.identity);
+            }
+
+            else if (Input.GetMouseButton(0) && currentLine != null)
+            {
+                currentLine.SetPosition(mousePosition);
+            }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             GameObject[] captureLines = GameObject.FindGameObjectsWithTag("Line");
-            foreach(GameObject line in captureLines)
+            foreach (GameObject line in captureLines)
             {
                 GameObject.Destroy(line);
             }
         }
+
     }
 }
